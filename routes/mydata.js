a4a0e1ec92f0767356653021a5587aebca062fa8
S542362 <77766701+S542362@users.noreply.github.com>
2021-03-24 17:30:52 -0500

    file added

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var icecream =0; 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    icecream=icecream +1;
   res.render('mydata', { title : 'shabnam shaik' });
 });
 
