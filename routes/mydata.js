71bdddf0893a474aacb5b3bb8583839690612301
S542362 <77766701+S542362@users.noreply.github.com>
2021-03-24 17:48:04 -0500

    users file

--- routes/mydata.js
+++ routes/mydata.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var icecream =0; 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     icecream=icecream +1;
-  res.render('mydata', { title : 'shabnam shaik' });
+  res.render('mydata', { title : 'shabnam shaik'});
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var icecream=0;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  icecream = icecream+1;
+  res.send('User accesses are: '+icecream);
 });
 
 module.exports = router;
