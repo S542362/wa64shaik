922daf7d5b11640339bd5fcbfec3812b74681f9f
S542362 <77766701+S542362@users.noreply.github.com>
2021-03-24 23:33:44 -0500

    bonus files

--- routes/computation.js
+++ routes/computation.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     let randsha=Math.floor(Math.random()*100)
-  res.render('compute', {result:'exponent function applied to '+randsha+' is '+Math.exp(randsha)});
+  res.render('compute', {result:'atan function applied to '+randsha+' is '+Math.atan(randsha)});
 });
 
 module.exports = router;
